eee3b3ebce5c121cdd79687f5884519f8074c720
selit777 <selit777maxim@gmail.com>
2024-09-11 08:00:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,18 +10,18 @@ if (getCookie('name') != undefined){
 }else {
   var a = 0;
 }
-if (getCookie('kas') != undefined){
-  var kas = [Number(getCookie('kas').split(' ')[0]), Number(getCookie('kas').split(' ')[1]), Number(getCookie('kas').split(' ')[2]), Number(getCookie('kas').split(' ')[3]), Number(getCookie('kas').split(' ')[4]), Number(getCookie('kas').split(' ')[5]), Number(getCookie('kas').split(' ')[6]), Number(getCookie('kas').split(' ')[7]), Number(getCookie('kas').split(' ')[8])];
-  document.getElementById('p').innerHTML = getCookie('kas');
-}else {
+// if (getCookie('kas') != undefined){
+//   var kas = [Number(getCookie('kas').split(' ')[0]), Number(getCookie('kas').split(' ')[1]), Number(getCookie('kas').split(' ')[2]), Number(getCookie('kas').split(' ')[3]), Number(getCookie('kas').split(' ')[4]), Number(getCookie('kas').split(' ')[5]), Number(getCookie('kas').split(' ')[6]), Number(getCookie('kas').split(' ')[7]), Number(getCookie('kas').split(' ')[8])];
+//   document.getElementById('p').innerHTML = getCookie('kas');
+// }else {
   var kas = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-}
-if (getCookie('up') != undefined){
-  var up = [Number(getCookie('up').split(' ')[0]), getCookie('up').split(' ')[1]), Number(getCookie('up').split(' ')[2]), Number(getCookie('up').split(' ')[3]), Number(getCookie('up').split(' ')[4]), Number(getCookie('up').split(' ')[5]), Number(getCookie('up').split(' ')[6]), Number(getCookie('up').split(' ')[7]), Number(getCookie('up').split(' ')[8])];
-  document.getElementById('p').innerHTML = getCookie('up');
-}else {
+// }
+// if (getCookie('up') != undefined){
+//   var up = [Number(getCookie('up').split(' ')[0]), getCookie('up').split(' ')[1]), Number(getCookie('up').split(' ')[2]), Number(getCookie('up').split(' ')[3]), Number(getCookie('up').split(' ')[4]), Number(getCookie('up').split(' ')[5]), Number(getCookie('up').split(' ')[6]), Number(getCookie('up').split(' ')[7]), Number(getCookie('up').split(' ')[8])];
+//   document.getElementById('p').innerHTML = getCookie('up');
+// }else {
   var up = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-}
+// }
 function Sum(kas) {
   document.cookie = "kas="+kas[0]+" "+kas[1]+" "+kas[2]+" "+kas[3]+" "+kas[4]+" "+kas[5]+" "+kas[6]+" "+kas[7]+" "+kas[8];
   document.cookie = "up="+up[0]+" "+up[1]+" "+up[2]+" "+up[3]+" "+up[4]+" "+up[5]+" "+up[6]+" "+up[7]+" "+up[8]+" ";
